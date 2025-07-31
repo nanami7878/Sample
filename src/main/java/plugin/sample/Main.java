1ab28736d67d6ae71487c19ad7ea55bcff75950e
Nanami Saito <nanami@Saito-NanamiMacBook-Air.local>
2025-08-01 01:36:35 +0900

    花火打ち上げ時のメッセージ表示を 「花火打ち上げ」から「たーまやー」に変更

--- src/main/java/plugin/sample/Main.java
+++ src/main/java/plugin/sample/Main.java
@@ -69,7 +69,7 @@ public final class Main extends JavaPlugin implements Listener {
       }
 
       Path path=Path.of("firework.txt");
-      Files.writeString(path,"花火打ち上げ");
+      Files.writeString(path,"たーまやー");
       player.sendMessage(Files.readString(path));
 
     }
